360217f5c11a3f37efb883c94b29fa174d132e16
Israt Jaman Jany <isratjamanjany@Israts-MacBook-Air.local>
2025-11-10 01:01:01 +0600

    update food done

--- index.js
+++ index.js
@@ -47,6 +47,34 @@ async function run() {
         result,
       })
     })
+      app.put("/foods/:id",async(req,res)=>{
+      const {id} = req.params
+      const data=req.body
+      const objectId = new ObjectId(id)
+        const filter = {_id: objectId}
+      const update={
+        $set:data
+      }
+      
+      
+      const result = await foodCollection.updateOne(filter,update)
+       res.send({
+        success:true,
+        result,
+      })
+    })
+
+    //delete
+        app.delete("/foods/:id",async(req,res)=>{
+      const {id} = req.params
+      const result = await foodCollection.deleteOne({_id: new ObjectId(id)})
+       res.send({
+        success:true,
+        result,
+      })
+    })
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
